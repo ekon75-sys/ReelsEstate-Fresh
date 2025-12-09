d2fb8fac1fa3bba7fb47f431860abcd76789ac25
ekon75-sys <ekon75@hotmail.com>
2025-12-09 18:03:47 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-(from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from motor.motor_asyncio import AsyncIOMotorClient
